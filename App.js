ad6a77dae79563bb582ffffa992d1fddc331b239
vknair@gmail.com <vknair@gmail.com>
2021-04-05 22:22:44 -0700

    Test

--- App.js
+++ App.js
@@ -1,36 +1,103 @@
-import React, { Component } from "react";
+import { Component, useReducer, useEffect, useMemo, createContext } from "react";
 import { Dimensions, SafeAreaView, StyleSheet } from "react-native";
 import LoginPage from "./Screens/LoginPage";
 import HomePage from "./Screens/HomePage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-let deviceWidth = Dimensions.get("window").width;
-let deviceHeight = Dimensions.get("window").width;
+import { getItemAsync } from 'expo-secure-store';
 
 const Stack = createStackNavigator();
+export const AuthContext = createContext();
+
 export default class App extends Component {
   constructor() {
     super();
-    // fix navigation thingy
+    const [state, dispatch] = useReducer(
+      (prevState, action) => {
+        switch(action.type) {
+          case 'RESTORE_TOKEN':
+            return {
+              ...prevState,
+              userToken: action.token,
+              isLoading: false
+            };
+          case 'SIGN_IN':
+            return {
+              ...prevState,
+              isSignout: false,
+              userToken: action.token
+            };
+          case 'SIGN_OUT':
+            return {
+              ...prevState,
+              isSignout: true,
+              userToken: null
+            };
+        }
+      },
+      {
+        isLoading: true,
+        isSignout: false,
+        userToken: null
+      }
+    );
+    useEffect(() => {
+      // Fetch the token from storage then navigate to our appropriate place
+      const bootstrapAsync = async () => {
+        let userToken;
+  
+        try {
+          userToken = await getItemAsync('userToken');
+        } catch (e) {
+          // Restoring token failed
+        }
+  
+        // After restoring token, we may need to validate it in production apps
+  
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+      };
+  
+      bootstrapAsync();
+    }, []);
   }
 
   render() {
+    const authContext = useMemo(
+      () => ({
+        signIn: async data => {
+          // In a production app, we need to send some data (usually username, password) to server and get a token
+          // We will also need to handle errors if sign in failed
+          // After getting token, we need to persist the token using `SecureStore`
+          // In the example, we'll use a dummy token
+  
+          dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+        },
+        signOut: () => dispatch({ type: 'SIGN_OUT' }),
+        signUp: async data => {
+          // In a production app, we need to send user data to server and get a token
+          // We will also need to handle errors if sign up failed
+          // After getting token, we need to persist the token using `SecureStore`
+          // In the example, we'll use a dummy token
+  
+          dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+        },
+      }),
+      []
+    );
     return (
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              options={{
-                headerShown: false,
-              }}
-              name="Login"
-              component={LoginPage}
-            />
-            <Stack.Screen name="Home" component={HomePage} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+      <AuthContext.Provider value={authContext}>
+        <Stack.Navigator>
+          {this.state.userToken == null ? (
+          <Stack.Screen
+            options={{ headerShown: false,}}
+            name="Login"
+            component={LoginPage}/>) : 
+            (<Stack.Screen name="Home" component={HomePage} />)
+          }
+        </Stack.Navigator>
+      </AuthContext.Provider>
     );
   }
 }
@@ -38,6 +105,5 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
   },
 });
